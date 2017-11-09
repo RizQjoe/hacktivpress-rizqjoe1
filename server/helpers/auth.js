f47a6520bcb928350788e97d7f2f24e7a144a77b
RizQjoe <rjoelisman@gmail.com>
2017-11-09 14:34:32 +0700

    update2

---
+++ server/helpers/auth.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
+function randomKey() {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var length = 8;
+  var result = "";
+
+  for (var i = length; i > 0; --i)
+    result += chars[Math.round(Math.random() * (chars.length - 1))];
+
+  return result;
+}
+
+function hash(data, key) {
+  const hash = crypto
+    .createHmac("sha256", key)
+    .update(data)
+    .digest("hex");
+  return hash;
+}
+
+function auth(req, res, next) {
+  const token = req.headers.token;
+  if (token) {
+    next();
+  } else {
+    res.send(" Please login Before");
+  }
+}
+
+module.exports = {
+  randomKey,
+  hash,
+  auth
+};
---
+++ server/routes/user.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/userController')
+
+router.get('/', (req,res)=>{res.send('hello user')})
+
+router.post('/register', controller.register)
+router.post('/login', controller.login)
+
+module.exports = router;
+
